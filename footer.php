e9ff29facab5d5bd8faa1dd4af2f06ceed150f61
alepelaez94 <alepelaez94@gmail.com>
2023-04-04 16:21:06 -0400

    js

--- footer.php
+++ footer.php
@@ -29,7 +29,8 @@
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-<script src="js/main.js"></script>
+<script src="<?php echo get_template_directory_uri() ?>js/main.js"></script>
 </body>
 </html>
