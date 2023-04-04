5542bfdcd9ce8cb924f3d0eeda65560371d3d721
alepelaez94 <alepelaez94@gmail.com>
2023-04-04 16:26:29 -0400

    a

--- functions.php
+++ functions.php
@@ -140,8 +140,11 @@ add_action( 'widgets_init', 'alepelaez_widgets_init' );
 function alepelaez_scripts() {
 	wp_enqueue_style( 'alepelaez-style', get_stylesheet_uri(), array(), _S_VERSION );
 	wp_style_add_data( 'alepelaez-style', 'rtl', 'replace' );
+	
+	wp_enqueue_style( 'sass-style', get_template_directory_uri() . '/sass.css', array(), _S_VERSION );
 
 	wp_enqueue_script( 'alepelaez-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script( 'alepelaez-main', get_template_directory_uri() . '/js/main.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
