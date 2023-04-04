02bbcda0aac0d41c8c0a64d05a2866ae596a34fa
alepelaez94 <alepelaez94@gmail.com>
2023-04-04 16:06:22 -0400

    bootstrap

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/sass.css">
 
 	<?php wp_head(); ?>
