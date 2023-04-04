97cacb644c2ff2e9cf3df5f1b08721091df83257
alepelaez94 <alepelaez94@gmail.com>
2023-04-04 19:15:36 -0400

    a

--- footer.php
+++ footer.php
@@ -32,8 +32,8 @@
 
   <!-- Copyright -->
   <div class="text-center text-white p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-	© 2020 Copyright:
-	<a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+	© 2023 Copyright:
+	<a class="text-white" href="https://mdbootstrap.com/">Alejandro Pelaez</a>
   </div>
   <!-- Copyright -->
 </footer>
