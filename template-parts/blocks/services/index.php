ad8d8d0a97864e9e76f93c128a78c2dc358e0ccf
alepelaez94 <alepelaez94@gmail.com>
2023-04-05 14:35:52 -0400

    a

---
+++ template-parts/blocks/services/index.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<div class="row">
+<?php if ( have_rows( 'services' ) ) : ?>
+    <?php while ( have_rows( 'services' ) ) : the_row(); ?>
+  <div class="col-sm-6">
+  <div class="card" style="width: 18rem;">
+  <img src="<?php the_sub_field( 'image' ); ?>" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title"><?php the_sub_field( 'title' ); ?></h5>
+    <p class="card-text"><?php the_sub_field( 'description' ); ?></p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+  </div>
+  <?php endwhile; ?>
+  <?php endif; ?>
+</div>
