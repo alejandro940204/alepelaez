498f7cb67ae39e188d2ebf250d8bc4ef4365d1c1
alepelaez94 <alepelaez94@gmail.com>
2023-05-08 22:02:35 -0400

    a

--- functions.php
+++ functions.php
@@ -1,1 +1,16 @@
 <?php
+add_filter('template_include', 'custom_team_template', 99);
+
+function custom_team_template($template) {
+    global $post;
+
+    if ($post->post_type == 'teams' && is_single()) {
+        $new_template = locate_template(array('team-single.php'));
+
+        if (!empty($new_template)) {
+            return $new_template;
+        }
+    }
+
+    return $template;
+}
