9439b9a14b8f438d9506591e86b2b57ad205864e
alepelaez94 <alepelaez94@gmail.com>
2023-05-08 22:14:19 -0400

    a

--- team-single.php
+++ team-single.php
@@ -1,2 +1,28 @@
-<?php ?>
-<h1>Hello team</h1>
+<?php
+/*
+Template Name: Equipo (Single)
+Template Post Type: team
+*/
+get_header(); ?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+
+        <?php while ( have_posts() ) : the_post(); ?>
+
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <header class="entry-header">
+                    <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+                </header><!-- .entry-header -->
+
+                <div class="entry-content">
+                    <?php the_content(); ?>
+                </div><!-- .entry-content -->
+            </article><!-- #post-## -->
+
+        <?php endwhile; ?>
+
+    </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
