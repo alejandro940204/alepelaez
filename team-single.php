f678f34cca2fa2b6510aca0822f070434a16bd07
alepelaez94 <alepelaez94@gmail.com>
2023-05-08 22:26:47 -0400

    a

--- team-single.php
+++ team-single.php
@@ -15,14 +15,108 @@ get_header(); ?>
 
                 <div class="entry-content">
                     <?php the_content(); ?>
-                    <div class="wpb_column vc_column_container vc_col-sm-12">
-                        <div class="vc_column-inner">
-                            <div class="wpb_wrapper">
-                                <div class="wpb_text_column wpb_content_element ">
-                                    <div class="wpb_wrapper">
-                                        <h2 style="text-align: center;">TEAM</h2>
-                                        <p style="text-align: center;"><a href="/team/">back to Teams CDR Companies</a></p>
+                    <div class="vc_row wpb_row vc_row-fluid">
+                        <div class="wpb_column vc_column_container vc_col-sm-12">
+                            <div class="vc_column-inner">
+                                <div class="wpb_wrapper">
+                                    <div class="templatera_shortcode">
+                                        <style type="text/css" data-type="vc_shortcodes-custom-css">
+                                            .vc_custom_1662741353078 {
+                                                padding-top: 20px !important;
+                                                padding-bottom: 20px !important;
+                                            }
 
+                                            .vc_custom_1662477791170 {
+                                                margin-top: 0px !important;
+                                                margin-bottom: 0px !important;
+                                                padding-top: 0px !important;
+                                                padding-bottom: 0px !important;
+                                            }
+
+                                            .vc_custom_1662583578337 {
+                                                margin-top: 0px !important;
+                                                margin-bottom: 0px !important;
+                                                padding-top: 6px !important;
+                                                padding-bottom: 6px !important;
+                                                background-color: #84dade !important;
+                                            }
+
+                                            .vc_custom_1662583585540 {
+                                                margin-top: 0px !important;
+                                                margin-bottom: 0px !important;
+                                                padding-top: 6px !important;
+                                                padding-bottom: 6px !important;
+                                                background-color: #007078 !important;
+                                            }
+
+                                            .vc_custom_1662583593309 {
+                                                margin-top: 0px !important;
+                                                margin-bottom: 0px !important;
+                                                padding-top: 6px !important;
+                                                padding-bottom: 6px !important;
+                                                background-color: #00a0df !important;
+                                            }
+
+                                            .vc_custom_1662583600061 {
+                                                margin-top: 0px !important;
+                                                margin-bottom: 0px !important;
+                                                padding-top: 6px !important;
+                                                padding-bottom: 6px !important;
+                                                background-color: #eba900 !important;
+                                            }
+
+                                            .vc_custom_1662583607561 {
+                                                margin-top: 0px !important;
+                                                margin-bottom: 0px !important;
+                                                padding-top: 6px !important;
+                                                padding-bottom: 6px !important;
+                                                background-color: #e43d30 !important;
+                                            }
+                                        </style>
+                                        <div class="vc_row wpb_row vc_row-fluid vc_custom_1662741353078 vc_row-o-content-middle vc_row-flex ult-vc-hide-row vc_row-has-fill" style="position: relative;" data-rtl="false" data-row-effect-mobile-disable="true">
+                                            <div class="upb_row_bg" data-upb_br_animation="" style="background: -webkit-linear-gradient(top, rgb(235, 235, 235) 0%, rgb(230, 230, 230) 99%); min-width: 1456px; left: -113px; width: 1456px;" data-bg-override="full"></div>
+                                            <div class="wpb_column vc_column_container vc_col-sm-12">
+                                                <div class="vc_column-inner">
+                                                    <div class="wpb_wrapper">
+                                                        <div class="wpb_text_column wpb_content_element ">
+                                                            <div class="wpb_wrapper">
+                                                                <h2 style="text-align: center;">TEAM</h2>
+                                                                <p style="text-align: center;"><a href="/team/">back to Teams CDR Cßompanies</a></p>
+
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div><!-- Row Backgrounds -->
+                                        <div data-vc-full-width="true" data-vc-full-width-init="true" data-vc-stretch-content="true" class="vc_row wpb_row vc_row-fluid vc_custom_1662477791170 vc_row-no-padding vc_row-o-content-bottom vc_row-flex" style="position: relative; box-sizing: border-box; width: 1456px;">
+                                            <div class="wpb_column vc_column_container vc_col-sm-1/5 vc_col-xs-1/5 vc_col-has-fill">
+                                                <div class="vc_column-inner vc_custom_1662583578337">
+                                                    <div class="wpb_wrapper"></div>
+                                                </div>
+                                            </div>
+                                            <div class="wpb_column vc_column_container vc_col-sm-1/5 vc_col-xs-1/5 vc_col-has-fill">
+                                                <div class="vc_column-inner vc_custom_1662583585540">
+                                                    <div class="wpb_wrapper"></div>
+                                                </div>
+                                            </div>
+                                            <div class="wpb_column vc_column_container vc_col-sm-1/5 vc_col-xs-1/5 vc_col-has-fill">
+                                                <div class="vc_column-inner vc_custom_1662583593309">
+                                                    <div class="wpb_wrapper"></div>
+                                                </div>
+                                            </div>
+                                            <div class="wpb_column vc_column_container vc_col-sm-1/5 vc_col-xs-1/5 vc_col-has-fill">
+                                                <div class="vc_column-inner vc_custom_1662583600061">
+                                                    <div class="wpb_wrapper"></div>
+                                                </div>
+                                            </div>
+                                            <div class="wpb_column vc_column_container vc_col-sm-1/5 vc_col-xs-1/5 vc_col-has-fill">
+                                                <div class="vc_column-inner vc_custom_1662583607561">
+                                                    <div class="wpb_wrapper"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="vc_row-full-width vc_clearfix"></div>
                                     </div>
                                 </div>
                             </div>
