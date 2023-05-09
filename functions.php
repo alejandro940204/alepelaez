b62508d09d4982380f43b5d463c12d2c3fd3c294
alepelaez94 <alepelaez94@gmail.com>
2023-05-09 11:55:58 -0400

    a

--- functions.php
+++ functions.php
@@ -72,4 +72,42 @@ add_action( 'acf/include_fields', function() {
 	) );
 } );
 
+//--------------- UPDATE Team Memebers --------------------------------
+add_action( 'rest_api_init', function () {
+    
+    add_filter( 'rest_authentication_errors', function( $result ) {
+        if ( ! empty( $result ) ) {
+            return $result;
+        }
+        if ( ! is_user_logged_in() ) {
+            return new WP_Error( 'rest_not_logged_in', 'No estás conectado.', array( 'status' => 401 ) );
+        }
+        return $result;
+    });
+   
+  });
+
+  function sincronizar_entradas($entrada_id, $sitio1_url, $sitio2_url) {
+    
+    $response = wp_remote_get($sitio1_url . '/wp-json/wp/v2/dt_team/' . $entrada_id);
+    $entrada1 = json_decode(wp_remote_retrieve_body($response), true);
+    
+    $response = wp_remote_post($sitio2_url . '/wp-json/wp/v2/dt_team/' . $entrada_id, array(
+        'method' => 'POST',
+        'headers' => array('Content-Type' => 'application/json'),
+        'body' => json_encode($entrada1)
+    ));
+}
+
+add_action('save_post', function ($post_id) {
+    // Verifica si la entrada es del CPT "team"
+    if (get_post_type($post_id) == 'dt_team') {
+        // Sincroniza la entrada en el primer sitio
+        sincronizar_entradas($post_id, 'https://alejandro.the-webones.com/', 'https://jorges169.sg-host.com/');
+    }
+}, 10, 1);
+
+
+
+  
 
