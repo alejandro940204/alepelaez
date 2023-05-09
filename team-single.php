c8af4478e5db94a71501265b807280d02a4a5497
alepelaez94 <alepelaez94@gmail.com>
2023-05-08 23:58:49 -0400

    a

--- team-single.php
+++ team-single.php
@@ -8,19 +8,21 @@ get_header(); ?>
 <div id="primary" class="content-area">
     <main id="main" class="site-main">
 
-        <?php while (have_posts()) : the_post(); 
-        $team_id=get_the_ID();
+        <?php while (have_posts()) : the_post();
+            $team_id = get_the_ID();
+            $image = wp_get_attachment_image_src(get_post_thumbnail_id($team_id), 'large');
+            $url = $image[0];
         ?>
 
             <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
                 <div class="entry-content">
                     <?php the_content(); ?>
-                    <div class="px-4 py-5 text-center team-hero">                       
+                    <div class="px-4 py-5 text-center team-hero">
                         <h1 class="display-5 fw-bold text-body-emphasis">TEAM</h1>
-                        <div class="col-lg-6 mx-auto">                           
+                        <div class="col-lg-6 mx-auto">
                             <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                            <a href="/team/">back to Teams CDR Companies</a>
+                                <a href="/team/">back to Teams CDR Companies</a>
                             </div>
                         </div>
                     </div>
@@ -33,31 +35,35 @@ get_header(); ?>
                         <div class="col col-1" style="background-color:#E43D31 ;"></div>
                     </div>
                     <div class="container col-md-8 team-single text-center">
-                        <img width="93" height="94" src="<?php get_the_post_thumbnail( $team_id, 'large' ); ?>" class="vc_single_image-img attachment-full" alt="" decoding="async" loading="lazy" title="Carlos-Duart" data-dt-location="https://alejandro.the-webones.com/carlos-duart-2/">
-                        <h3 style="text-align: center;margin: 20px;"><?php get_the_title() ?></h3>
-                        <h5 style="text-align: center;margin: 20px;"><?php the_field('position',$team_id) ?></h5>
+                        <img width="93" height="94" src="<?php echo $url ?>" class="vc_single_image-img attachment-full" alt="" decoding="async" loading="lazy" title="Carlos-Duart" data-dt-location="https://alejandro.the-webones.com/carlos-duart-2/">
+                        <h3 style="text-align: center;margin: 20px;"><?php get_post_type() ?></h3>
+                        <h5 style="text-align: center;margin: 20px;"><?php the_field('position', $team_id) ?></h5>
                         <div class="team-bio text-left">
-                        <?php the_field('bio',$team_id) ?>
+                            <?php the_field('bio', $team_id) ?>
                         </div>
                     </div>
 
                     <style>
-                        .team-single{
+                        .team-single {
                             padding-top: 70px;
                         }
+
                         .team-bio {
                             border-top: 1px solid black;
                             padding-top: 50px;
                         }
-                        .team-hero{
-                           background-color: #E9E9E9;
+
+                        .team-hero {
+                            background-color: #E9E9E9;
                         }
-                        .team-colors{
+
+                        .team-colors {
                             margin: 0 auto;
                         }
-                        .team-colors .col{
+
+                        .team-colors .col {
                             height: 12px;
-                           
+
                         }
                     </style>
                 </div><!-- .entry-content -->
