91ae607fff16a3d194caa8f8144a2532edf99d7d
alepelaez94 <alepelaez94@gmail.com>
2023-05-15 19:13:36 -0400

    a

--- team-single.php
+++ team-single.php
@@ -22,7 +22,7 @@ get_header(); ?>
                         <h1 class="display-5 fw-bold text-body-emphasis">TEAM</h1>
                         <div class="col-lg-6 mx-auto">
                             <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                                <a href="/team/">back to Teams CDR Companies</a>
+                                <a style="color:black;" href="/team/">back to Teams CDR Companies</a>
                             </div>
                         </div>
                     </div>
